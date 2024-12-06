3f1f8e44adad2ecd274d3ba6b8883e5557119180
ANAS BANAT <anasbanat.93.ab@gmail.com>
2024-12-06 04:08:14 +0300

    update

--- src/app/conferences/page.tsx
+++ src/app/conferences/page.tsx
@@ -1,12 +1,6 @@
 import About from "../../components/About";
 import Breadcrumb from "../../components/Common/Breadcrumb";
 import Team from "../../components/Team";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "About Us",
-  description: "Learn more about our company and team.",
-};
 
 
 const AboutPage = () => {
--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,12 +1,6 @@
 import Breadcrumb from "../../components/Common/Breadcrumb";
 import Faq from "../../components/Faq";
-import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title:
-    "FAQ Page",
-  description: "This is FAQ page description",
-};
 
 const ContactPage = () => {
   return (
